8e83cc21fd470e09621841094948f75cf23c3761
Tyler <tyler.obrien1@gmail.com>
2018-05-10 16:48:56 -0700

    added the next button

--- src/components/Album.js
+++ src/components/Album.js
@@ -53,6 +53,14 @@ class Album extends Component {
 		this.play();
 	}
 
+	handleNextClick(){
+		const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+		const newIndex = Math.min(this.state.album.songs.length, currentIndex + 1);
+		const newSong = this.state.album.songs[newIndex];
+		this.setSong(newSong);
+		this.play();
+	}
+
 	render() {
 		return (
 			<section className="album">
@@ -94,6 +102,7 @@ class Album extends Component {
 					currentSong={this.state.currentSong}
 					handleSongClick={() => this.handleSongClick(this.state.currentSong)}
 				 	handlePrevClick={() => this.handlePrevClick()}
+				 	handleNextClick={() => this.handleNextClick()}
 				 />
 			</section>
 			);
