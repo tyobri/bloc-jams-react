27befe5269a841bc5b3140f105c549b468c112a0
Tyler <tyler.obrien1@gmail.com>
2018-06-12 11:35:05 -0700

    added formatTime to the song duration

--- src/components/Album.js
+++ src/components/Album.js
@@ -134,7 +134,7 @@ class Album extends Component {
 										</button>
 									</td>
 									<td className="song-title">{song.title}</td>
-									<td className="song-duration">{song.duration}</td>
+									<td className="song-duration">{this.formatTime(song.duration)}</td>
 								</tr>
 								)}
 					</tbody>
